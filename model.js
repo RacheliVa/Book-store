2a254194d051836e1df25847bdcdae15efa8772f
RacheliVa <racheli.litman@gmail.com>
2024-10-10 09:20:26 +0300

    Adding files

---
+++ model.js
@@ -0,0 +1,38 @@
+const dump = [
+    {
+        id: 0,
+        bookName: "פיצי לומד להזהר",
+        price: 15,
+        img: "https://www.sifreiorhachaim.co.il/wp-content/uploads/2024/01/2570466.jpg",
+        rate: 0
+    },
+    {
+        id: 1,
+        bookName: "פיצי משמח את אמא",
+        price: 20,
+        img: "https://www.sifreiorhachaim.co.il/wp-content/uploads/2020/11/2570465-300x163.jpg",
+        rate: 0
+    },
+    {
+        id: 2,
+        bookName: "פיצי מדבר אמת",
+        price: 18,
+        img: "https://www.sifreiorhachaim.co.il/wp-content/uploads/2024/01/2570468.jpg",
+        rate: 0
+    },
+    {
+        id: 3,
+        bookName: "פיצי שומר על חפציו",
+        price: 22,
+        img: "https://www.sifreiorhachaim.co.il/wp-content/uploads/2020/11/2080701-1-270x300.jpg",
+        rate: 0
+    },
+    {
+        id: 4,
+        bookName: "מה קרה לפיצי",
+        price: 13,
+        img: "https://www.sifreiorhachaim.co.il/wp-content/uploads/2020/11/2080512-1-267x300.jpg",
+        rate: 0
+    },
+
+]
